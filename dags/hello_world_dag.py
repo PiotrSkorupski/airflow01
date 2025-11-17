63e566aec94f316baab3075336fb782dc37c6986
unknown <piotr.skorupski@elitmind.com>
2025-11-17 17:39:36 +0000

    Initial commit

---
+++ dags/hello_world_dag.py
@@ -0,0 +1,25 @@
+
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+# Funkcja, którą wykona zadanie
+def hello_world():
+    print("Dag executer from --main--")
+
+# Definicja DAG
+with DAG(
+    dag_id="hello_world_dag",
+    start_date=datetime(2025, 11, 14),
+    schedule_interval="@daily",  # uruchamiane codziennie
+    catchup=False,
+    tags=["example"]
+) as dag:
+
+    # Zadanie
+    hello_task = PythonOperator(
+        task_id="say_hello",
+        python_callable=hello_world
+    )
+
+    hello_task
